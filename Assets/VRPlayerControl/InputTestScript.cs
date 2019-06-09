dfaf0d1c65af85097ac64d6393d2b10bb98e217f
Andres <tiredamage42@icloud.com>
2019-06-09 13:55:05 -0400

    added bindings test

--- Assets/VRPlayerControl/InputTestScript.cs
+++ Assets/VRPlayerControl/InputTestScript.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 
 public class InputTestScript : MonoBehaviour
 {
+    
+    public SteamVR_Action_Single squeezeAction;
+    public SteamVR_Action_Vector2 touchpadAction;
     // Start is called before the first frame update
     void Start()
     {
+        
         
     }
 
