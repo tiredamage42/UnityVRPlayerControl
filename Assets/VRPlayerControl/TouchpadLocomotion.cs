8d89377775dd0e0fdb744c0c3a78e66a0cb1f0cd
Andres Gomez <andresgomezjr89@gmail.com>
2019-06-10 17:53:30 -0400

    added vignetting, different turn types and running

--- Assets/VRPlayerControl/TouchpadLocomotion.cs
+++ Assets/VRPlayerControl/TouchpadLocomotion.cs
@@ -7,15 +7,101 @@ using Valve.VR.InteractionSystem;
 namespace VRPlayer {
 
 
-/*
-    add this to character controller gameobject
 
+
+
+/*
+
+motion sickness avoid:
+
+
+    avoid repeated patterns in environment (checkerboard tiles, etc..)
+
+    
+    dont move camera wihtout player input
+
+    remove accelerations in movement
+
+    make 3d hud (so eyes dont have to refocus from 2d to 3d)
+
+    look up walk in place solutions (https://www.youtube.com/watch?v=IQ_jgnwHwFA)
+    http://smirkingcat.software/ripmotion/
+
+
+https://cubic9.com/Devel/OculusRift/BobbingBots_en/ (another bob walk in place solution)
+
+
+
+
+    https://www.youtube.com/watch?v=Ti6kxSDTI8g (walking with hand movements (grab and drag))
+    (takes 3 frames to go up step)
+
+    follow redirected walking
+
+    choose rotation when teleporting
+
+    teleport grenade?
+
+
+
+    walkabout method:
+        reach edge, and hold button (blurs image and i guess stops tracking)
+        turn around in meatspace while camera stays
+
+
+
+
+climb up walls, click and drag
+
+
+
+a decrease in FPS (frames per second) animation of camera motion to 10-15 FPS, while the world is rendered at a frequency of 90 Hz, may help. 
+In this case, the brain does not recognize the movement as such and, therefore, dissonance is avoided.
+
+you could use this technique for jumping. Make the frame at the beginning, middle, and at the top part 
+of the trajectory. And, instead of Dash Teleport, only use the starting, middle, and end 
+positions in order to make the key animation frames, and just get rid of the rest.
+
+
+
+
+darker colors (80/20) (dark/light)
+
+
+"grab and turn" https://www.youtube.com/watch?v=oSP5sjg9TIs
+
+maybe blur isntead of vignette
+
+
+move really fast towards teleport point (tested and works i guess.) maybe up FOV (to give it a smear look)
+(hsould take <= 100 ms, thats the time it takes for the brain to get sick)
+
+
+amplified walking (move tracking space along with camera movement vector so meatspace steps go farther)
+ gain walking"
+
+ rectangular area without gain, then add gain around it
+
+
+    TODO:
+        xray transparent shader when camera clips trhoug object
+
+        add nose (or simulated) in between eyes (static frame of reference helps motion sickness)
+
+*/
+
+
+ 
+ 
+
+
+/*
 
     make camera rig child of this transform (at origin)
 
+    this moves the play area around with the character controller
 
-    DONT MOVE TRANSFORM (player uses it as tracking origin, so it should be offset)
-
+    rotates the play area around the camera when rotating
 */
 
 
@@ -24,14 +110,23 @@ namespace VRPlayer {
     
 public class TouchpadLocomotion : MonoBehaviour
 {
-    public float turnSpeed = 5;
 
-    public float deadZone = .1f;
+    public float comfortVignetteIntensity = 1.0f;
 
-    public SteamVR_Action_Vector2 trackpackAxisAction;
+    // public float turnSpeed = 5;
+    // public float deadZone = .1f;
+
     public SteamVR_Action_Boolean moveEnableAction;
+    public SteamVR_Action_Vector2 moveAction;
+
+    public SteamVR_Action_Boolean turnActionLeft, turnActionRight;
+    public SteamVR_Action_Boolean runAction;
+
+    
     public SteamVR_Input_Sources moveHand;
-    public SteamVR_Input_Sources lookHand;
+    public SteamVR_Input_Sources turnHand;
+    
+    // public SteamVR_Action_Vector2 trackpackAxisAction;
     
     
     // public float sensitivity = 0.1f;
@@ -40,7 +135,7 @@ public class TouchpadLocomotion : MonoBehaviour
     // public Transform cameraRig, head;
     public Transform head;
     
-    float currentSpeed;
+    // float currentSpeed;
     CharacterController characterController;
 
 
@@ -48,11 +143,11 @@ public class TouchpadLocomotion : MonoBehaviour
 
     
     // Start is called before the first frame update
-    void Start()
-    {
+    // void Start()
+    // {
         // cameraRig = SteamVR_Render.Top().origin;
         // head = SteamVR_Render.Top().head;
-    }
+    // }
 
     // Update is called once per frame
     void Update()
@@ -63,11 +158,16 @@ public class TouchpadLocomotion : MonoBehaviour
         // }
 
         // else {
-            Method2Update(Time.deltaTime);
+            // Method2Update(Time.deltaTime);
         // }
 
-     
-        
+        InputUpdateLoop();
+    }
+
+
+
+    void FixedUpdate () {
+        FixedUpdateLoop(Time.fixedDeltaTime);
     }
 
 
@@ -93,16 +193,193 @@ public class TouchpadLocomotion : MonoBehaviour
 
 
     /*
+    */
     
 
+    public float turnDegrees = 45f;    
+
+    public float fastTurnTime = .25f;
+
+    float fastTurnDegreesPerSecond {
+        get {
+            return turnDegrees / fastTurnTime;
+        }
+    }
     
-    
-     */
 
     // makes sure head doesnt rotate with cc
-    float currentTurnSpeed;
-    public float turnSpeedAccelSpeed = 15;
+    // float currentTurnSpeed;
+    public float smoothTurnSpeedAcceleration = 5;
+    public float smoothTurnSpeed = 40;
 
+    public enum TurnType {
+        None, Instant, Fast, Smooth
+    };
+
+    public TurnType turnType;
+
+
+    void DoInstantTurn (float degrees, bool toRight) {
+        transform.RotateAround(head.position, Vector3.up, toRight ? degrees : -degrees);
+    }
+
+
+    float fastTurnTotalRotation = float.MaxValue;
+    bool fastTurnRight;
+    void DoFastTurn (bool toRight) {
+        fastTurnTotalRotation = 0;
+        fastTurnRight = toRight;
+    }
+
+    bool HandleFastTurn (float targetDegrees, float degreesPerSecond, bool toRight, float deltaTime) {
+        
+        //keep turning if we're not there yet
+        if(Mathf.Abs(fastTurnTotalRotation) < targetDegrees) {
+            
+            float rotationAdd = degreesPerSecond * deltaTime * (toRight ? 1 : -1);
+            transform.RotateAround(head.position, Vector3.up, rotationAdd);
+            fastTurnTotalRotation += rotationAdd;
+
+            return true;
+        }
+        return false;
+    }
+
+    float smoothTurnMultiplier;
+
+    bool HandleSmoothTurn (int turn, float deltaTime) {
+        smoothTurnMultiplier = Mathf.Lerp(smoothTurnMultiplier, turn, deltaTime * smoothTurnSpeedAcceleration);
+        if (smoothTurnMultiplier != 0) {
+            transform.RotateAround(head.position, Vector3.up, smoothTurnMultiplier * smoothTurnSpeed * deltaTime);
+            return true;
+
+
+        }
+        return false;
+    }
+
+    int smoothTurnDirection;
+
+    void HandleTurnInput (bool movementEnabled) {
+        smoothTurnDirection = 0;
+     
+        if (!movementEnabled)
+            return;
+
+        switch(turnType) {
+            case TurnType.None:
+                break;
+            case TurnType.Instant:
+                if (turnActionLeft.GetStateDown(turnHand)) {
+                    DoInstantTurn(turnDegrees, false);
+                }
+                else if (turnActionRight.GetStateDown(turnHand)) {
+                    DoInstantTurn(turnDegrees, true);
+                }
+                break;
+            case TurnType.Fast:            
+                if (turnActionLeft.GetStateDown(turnHand)) {
+                    DoFastTurn(false);
+                }
+                else if (turnActionRight.GetStateDown(turnHand)) {
+                    DoFastTurn(true);
+                }
+                break;
+            case TurnType.Smooth:
+                if (turnActionLeft.GetState(turnHand)) {
+                    smoothTurnDirection = -1;
+                }
+                else if (turnActionRight.GetState(turnHand)) {
+                    smoothTurnDirection = 1;
+                }
+                break;
+        }
+    }
+
+    public float runMultiplier = 2;
+
+    bool isRunning;
+
+    // any movement above this angle with forward movement cant run.
+    const float runAngleThreshold = 45f; 
+
+    public enum RunMode { None, Held, Clicked };
+    public RunMode runMode;
+
+    Vector2 currentMoveVector;
+
+
+    // set move velocity to zero every frame for instant stop
+    // when not moving
+    void HandleMoveInput (bool movementEnabled) {
+        currentMoveVector = Vector2.zero;
+        
+        if (!movementEnabled) {
+            isRunning = false;
+            return;
+        }
+        currentMoveVector = moveAction.GetAxis(moveHand);
+
+        if (currentMoveVector != Vector2.zero) {
+
+            if (currentMoveVector.sqrMagnitude > 1) {
+                Debug.LogError("NORMALIZING");
+                currentMoveVector.Normalize();
+            }
+
+            if (runMode != RunMode.None) {
+
+                bool runPossible = Vector2.Angle(currentMoveVector, new Vector2(0, 1)) <= runAngleThreshold;
+                if (!runPossible) {
+
+                    // we're used to walking forward, lateral and backwards/up down movement feels wrong
+                    // so it's speed needs to be clamped to prevent motion sickness
+
+                    isRunning = false;
+                }
+                else {
+                    if (runMode == RunMode.Held) {
+                        isRunning = runAction.GetState(moveHand);
+                    }
+                    else {
+                        if (runAction.GetStateDown(moveHand)) {
+                            isRunning = true;
+                        }
+                    }
+                    
+                    if (!isRunning) {
+                        
+                    }
+
+                }
+            }
+
+        }
+
+
+
+
+
+        currentMoveVector = currentMoveVector * maxSpeed * (isRunning ? runMultiplier : 1);
+    }
+
+        
+
+
+    bool HandleTurningUpdate(float deltaTime) {
+        switch(turnType) {
+            case TurnType.None: break;
+            case TurnType.Instant: break;
+            case TurnType.Fast:
+                return HandleFastTurn(turnDegrees, fastTurnDegreesPerSecond, fastTurnRight, deltaTime);
+            case TurnType.Smooth:
+                return HandleSmoothTurn (smoothTurnDirection, deltaTime);
+        }
+        return false;
+    }
+        
+        /*
+        
 
 
     void HandleHead (float deltaTime) {
@@ -129,13 +406,7 @@ public class TouchpadLocomotion : MonoBehaviour
         }
         currentTurnSpeed = Mathf.Lerp(currentTurnSpeed, isTurning ? (left ?  -1 : 1) : 0, deltaTime * turnSpeedAccelSpeed);
         if (currentTurnSpeed != 0) {
-
-        
-
             transform.RotateAround(oldCameraRigPosition, Vector3.up, currentTurnSpeed * turnSpeed * deltaTime);
-
-
-
         }
         else {
             // transform.rotation = Quaternion.Euler( new Vector3(0, oldCameraRigRotation.eulerAngles.y, 0) );
@@ -152,6 +423,8 @@ public class TouchpadLocomotion : MonoBehaviour
         //     head.rotation = oldCameraRigRotation;
         // }
     }
+     */
+
 
     // void CalculateMovement () {
     //     // figure out movement orientation
@@ -174,6 +447,39 @@ public class TouchpadLocomotion : MonoBehaviour
 
     //     characterController.Move(movement);
     // }
+
+    void InputUpdateLoop () {
+
+        bool movementEnabled = moveEnableAction.GetState(moveHand);
+        HandleTurnInput(movementEnabled);
+        HandleMoveInput(movementEnabled);
+    }
+    void FixedUpdateLoop (float deltaTime) {
+        bool isTurning = HandleTurningUpdate(deltaTime);
+        HandleCapsuleHeight();
+        MoveCharacterController(deltaTime);
+
+        bool isMoving = currentMoveVector != Vector2.zero;
+
+        if (isTurning || isMoving) {
+            SetComfortVignette(isTurning || isMoving ? comfortVignetteIntensity : 0);
+        }
+    }
+
+    void SetComfortVignette(float intensity) {
+
+        VignettingVR vignetting = head.GetComponentInChildren<VignettingVR>();
+        vignetting.vignettingIntensity = intensity;
+        
+    }
+
+    void MoveCharacterController (float deltaTime) {
+
+
+        Vector3 velocity = new Vector3( currentMoveVector.x, 0, currentMoveVector.y);
+
+        characterController.Move( transform.TransformDirection( velocity * deltaTime ) );
+    }
 
 
 
@@ -217,7 +523,7 @@ public class TouchpadLocomotion : MonoBehaviour
     // }
 
 
-    public Transform handTransform;
+    // public Transform handTransform;
 
 
     /*
@@ -229,95 +535,84 @@ public class TouchpadLocomotion : MonoBehaviour
     */
 
     
-     public static float Angle(Vector2 p_vector2)
-     {
-         if (p_vector2.x < 0)
-         {
-             return 360 - (Mathf.Atan2(p_vector2.x, p_vector2.y) * Mathf.Rad2Deg * -1);
-         }
-         else
-         {
-             return Mathf.Atan2(p_vector2.x, p_vector2.y) * Mathf.Rad2Deg;
-         }
-     }
+    //  public static float Angle(Vector2 p_vector2)
+    //  {
+    //      if (p_vector2.x < 0)
+    //      {
+    //          return 360 - (Mathf.Atan2(p_vector2.x, p_vector2.y) * Mathf.Rad2Deg * -1);
+    //      }
+    //      else
+    //      {
+    //          return Mathf.Atan2(p_vector2.x, p_vector2.y) * Mathf.Rad2Deg;
+    //      }
+    //  }
 
 
 
 
 
-    void Method2Update(float deltaTime)
-    {
-        HandleHead(deltaTime);
+    // void Method2Update(float deltaTime)
+    // {
+    //     HandleHead(deltaTime);
 
-        HandleCapsuleHeight();
+    //     HandleCapsuleHeight();
 
-        Vector2 currentTrackpad = trackpackAxisAction.GetAxis(moveHand);
+    //     Vector2 currentTrackpad = trackpackAxisAction.GetAxis(moveHand);
         
         
-        //Set size and position of the capsule collider so it maches our head.
-        // characterController.height = head.localPosition.y;
-        // characterController.center = new Vector3(head.localPosition.x, head.localPosition.y / 2, head.localPosition.z);
+    //     //Set size and position of the capsule collider so it maches our head.
+    //     // characterController.height = head.localPosition.y;
+    //     // characterController.center = new Vector3(head.localPosition.x, head.localPosition.y / 2, head.localPosition.z);
         
 
-        //get the angle of the touch and correct it for the rotation of the controller
-        // Vector3 moveDirection = Quaternion.AngleAxis(Angle(currentTrackpad) + handTransform.localRotation.eulerAngles.y, Vector3.up) * Vector3.forward;
+    //     //get the angle of the touch and correct it for the rotation of the controller
+    //     // Vector3 moveDirection = Quaternion.AngleAxis(Angle(currentTrackpad) + handTransform.localRotation.eulerAngles.y, Vector3.up) * Vector3.forward;
         
 
 
-        // if (GetComponent<Rigidbody>().velocity.magnitude < speed && trackpad.magnitude >Deadzone){//make sure the touch isn't in the deadzone and we aren't going to fast.
-        //     GetComponent<Rigidbody().AddForce(moveDirection * 30);
-        // }
+    //     // if (GetComponent<Rigidbody>().velocity.magnitude < speed && trackpad.magnitude >Deadzone){//make sure the touch isn't in the deadzone and we aren't going to fast.
+    //     //     GetComponent<Rigidbody().AddForce(moveDirection * 30);
+    //     // }
 
 
 
-        // Vector3 velocity = new Vector3(0,0,0);
-        if (currentTrackpad.magnitude > deadZone)
-        {
-            //make sure the touch isn't in the deadzone and we aren't going to fast.
-            // CapCollider.material = NoFrictionMaterial;
+    //     // Vector3 velocity = new Vector3(0,0,0);
+    //     if (currentTrackpad.magnitude > deadZone)
+    //     {
+    //         //make sure the touch isn't in the deadzone and we aren't going to fast.
+    //         // CapCollider.material = NoFrictionMaterial;
             
-            // velocity = currentTrackpad;// moveDirection;
+    //         // velocity = currentTrackpad;// moveDirection;
             
-            // if (JumpAction.GetStateDown(MovementHand) && GroundCount > 0)
-            // {
-            //     float jumpSpeed = Mathf.Sqrt(2 * jumpHeight * 9.81f);
-            //     RBody.AddForce(0, jumpSpeed, 0, ForceMode.VelocityChange);
-            // }
-            currentTrackpad = currentTrackpad * maxSpeed * deltaTime;
+    //         // if (JumpAction.GetStateDown(MovementHand) && GroundCount > 0)
+    //         // {
+    //         //     float jumpSpeed = Mathf.Sqrt(2 * jumpHeight * 9.81f);
+    //         //     RBody.AddForce(0, jumpSpeed, 0, ForceMode.VelocityChange);
+    //         // }
+    //         currentTrackpad = currentTrackpad * maxSpeed * deltaTime;
 
-            Vector3 velocityNew = new Vector3( 
-                currentTrackpad.x,// * maxSpeed,// - characterController.velocity.x, 
-                0, 
-                currentTrackpad.y// * maxSpeed //- characterController.velocity.z 
-            );
-
-
-            characterController.Move(
-                // maybe head
-                transform
-                    .TransformDirection( velocityNew ) 
-            );
+    //         Vector3 velocityNew = new Vector3( 
+    //             currentTrackpad.x,// * maxSpeed,// - characterController.velocity.x, 
+    //             0, 
+    //             currentTrackpad.y// * maxSpeed //- characterController.velocity.z 
+    //         );
 
 
+    //         characterController.Move(
+    //             // maybe head
+    //             transform
+    //                 .TransformDirection( velocityNew ) 
+    //         );
 
-
-
-
-
-
-            // RBody.AddForce(velocity.x*MovementSpeed - RBody.velocity.x, 0, velocity.z*MovementSpeed - RBody.velocity.z, ForceMode.VelocityChange);
-            // Debug.Log("Velocity" + velocity);
-            // Debug.Log("Movement Direction:" + moveDirection);
-        }
-        // else if(GroundCount > 0)
-        // {
-        //     CapCollider.material = FrictionMaterial;
-        // }
-    }
-
-
-
-
+    //         // RBody.AddForce(velocity.x*MovementSpeed - RBody.velocity.x, 0, velocity.z*MovementSpeed - RBody.velocity.z, ForceMode.VelocityChange);
+    //         // Debug.Log("Velocity" + velocity);
+    //         // Debug.Log("Movement Direction:" + moveDirection);
+    //     }
+    //     // else if(GroundCount > 0)
+    //     // {
+    //     //     CapCollider.material = FrictionMaterial;
+    //     // }
+    // }
 }
 
 
---
+++ Assets/VignettingVR/VignettingVR.cs
@@ -0,0 +1,75 @@
+﻿// http://www.julianheinken.com/
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Post-Effect for Vignetting in VR.
+/// </summary>
+[RequireComponent(typeof(Camera))]
+// [ExecuteInEditMode]
+public class VignettingVR : MonoBehaviour
+{
+    // const settings
+    const string DEBUG_VIGNETTING_MASK = "DEBUG_SHOW_VIGNETTE";
+    const float VIGNETTING_DEPTH = 10f;
+    //---------------------------------------
+    public bool vignettingEnable = true;
+    public float vignettingIntensity;
+    
+    public bool debugMode;
+    Vector3 vignetteWorldSpaceDirection;
+    Vector4[] viewportSpaceOffset = new Vector4[2];
+    Camera currentCamera;
+
+    private Material material;
+    [HideInInspector]
+    public Shader m_Shader;
+    void Start()
+    {
+        vignetteWorldSpaceDirection = transform.forward;
+        currentCamera = GetComponent<Camera>();
+        material = new Material(m_Shader);
+        material.hideFlags = HideFlags.HideAndDontSave;
+        viewportSpaceOffset = new Vector4[2];
+    }
+
+    Matrix4x4 GetWorldToClipMatrix(Camera.StereoscopicEye eye)
+    {
+        return currentCamera.GetStereoProjectionMatrix(eye) * currentCamera.GetStereoViewMatrix(eye);  // combine view and projection matrix
+    }
+
+    static Vector4 NormalizeScreenSpaceCords(Vector4 coords)
+    {
+        //normalize viewport coords from [-1, 1] [0, 1]
+        //we need to do this, since we are refereing to screenspace UV coords in 
+        return (coords * 0.5f) + Vector4.one * 0.5f;
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        material.DisableKeyword(DEBUG_VIGNETTING_MASK);
+
+        if (debugMode)
+        {
+            material.EnableKeyword(DEBUG_VIGNETTING_MASK);
+        }
+
+        Vector3 currentCameraForward = transform.forward;
+
+        //translate vignetting direction to worldspace position
+        Vector3 vignetteCenterWorldSpace = transform.position + currentCameraForward * VIGNETTING_DEPTH;
+        Matrix4x4 worldToClipSpace_left = GetWorldToClipMatrix(Camera.StereoscopicEye.Left);
+        viewportSpaceOffset[0] =  worldToClipSpace_left.MultiplyPoint(vignetteCenterWorldSpace);// get world-space position as clip-space position 
+        viewportSpaceOffset[0] = -NormalizeScreenSpaceCords(viewportSpaceOffset[0]);// convert clip-space [-1,1] to viewport space [0,1]. also negate the offset.
+
+        Matrix4x4 worldToClipSpace_right = GetWorldToClipMatrix(Camera.StereoscopicEye.Right);
+        viewportSpaceOffset[1] = worldToClipSpace_right.MultiplyPoint(vignetteCenterWorldSpace);// get world-space position as clip-space position 
+        viewportSpaceOffset[1] = -NormalizeScreenSpaceCords(viewportSpaceOffset[1]); // convert clip-space [-1,1] to viewport space [0,1]. also negate the offset.
+
+        // pass values to shader
+        material.SetVectorArray("_vignetteViewportSpaceOffset", viewportSpaceOffset);
+        material.SetFloat("_vignettingIntensity", vignettingEnable ? vignettingIntensity * 0.1f : 0f);
+        
+        Graphics.Blit(source, destination, material);
+    }
+}
