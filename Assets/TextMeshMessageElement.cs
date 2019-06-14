519e6fdba76244e21f98a8ecd2af1af549684cc6
Andres Gomez <andresgomezjr89@gmail.com>
2019-06-14 19:18:15 -0400

    pull error fixing

--- Assets/TextMeshMessageElement.cs
+++ Assets/TextMeshMessageElement.cs
@@ -1,29 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+namespace UIMessaging {
+    public class TextMeshMessageElement : MessageElement {
+        
+        public int fontSize = 120;
+        public float characterSize = .001f;
+        TextMesh messageText;
+        
+        protected override void SetMessage() {
+            
+            if (messageText == null)
+                messageText = gameObject.AddComponent<TextMesh>();
+            
+            messageText.fontSize = fontSize;
+            messageText.characterSize = characterSize;
+            messageText.alignment = textAlignment;
+            messageText.anchor = textAnchor;
+            messageText.color = color;
+            messageText.text = message;
+        }
 
-
-public class TextMeshMessageElement : MessageElement
-{
-
-    TextMesh messageText;
-    public int fontSize = 120;
-    public float characterSize = .001f;
-    
-    protected override void SetMessage() {
-        if (messageText == null)
-            messageText = gameObject.AddComponent<TextMesh>();
-        messageText.fontSize = fontSize;
-        messageText.characterSize = characterSize;
-        messageText.alignment = textAlignment;
-        messageText.anchor = textAnchor;
-        messageText.color = color;
-
-        messageText.text = message;
-    }
-    protected override void SetAlpha() {
-        Color c = messageText.color;
-        c.a = alpha;
-        messageText.color = c;
+        protected override void SetAlpha() {
+            Color c = messageText.color;
+            c.a = alpha;
+            messageText.color = c;
+        }
     }
 }
