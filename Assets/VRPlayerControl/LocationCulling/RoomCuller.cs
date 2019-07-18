cabb86f407669fce281fdd1b1d34a8fe63d3e5e5
Andres Gomez <andresgomezjr89@gmail.com>
2019-07-18 10:55:19 -0400

    added some room culling

---
+++ Assets/VRPlayerControl/LocationCulling/RoomCuller.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[RequireComponent(typeof(BoxCollider))]
+public class RoomCuller : MonoBehaviour
+{
+    public GameObject[] objectsHandled;
+    
+    const string playerTag = "Player";
+
+    void Awake () {
+        EnableObjects(false);
+    }
+
+    void EnableObjects (bool enabled) {
+        
+        for (int i = 0; i < objectsHandled.Length; i++) {
+            objectsHandled[i].SetActive(enabled);
+        }
+        state = enabled;
+    }
+    bool state;
+
+    void OnTriggerExit (Collider other) {
+        if (other.gameObject.CompareTag(playerTag)) {
+            Vector3 lPos = transform.InverseTransformPoint(other.transform.position);
+            bool enabled = lPos.z > 0;
+            if (state == enabled)
+                return;
+            EnableObjects (enabled);
+
+        }
+    }
+
+
+    BoxCollider box;
+    void OnDrawGizmos () {
+        if (box == null)
+            box = GetComponent<BoxCollider>();
+
+        Gizmos.color = new Color(0,0,1,.25f);
+        Matrix4x4 oldMatrix = Gizmos.matrix;
+        Gizmos.matrix = Matrix4x4.TRS(transform.position, transform.rotation, transform.localScale);
+        Gizmos.DrawCube(box.center, box.size);
+        Gizmos.matrix = oldMatrix;
+    }
+}
